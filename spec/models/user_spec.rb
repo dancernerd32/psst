a3ebc1bb32a0dc9da32cacf8ae73503dcc69e2fd
dancernerd32 <dancernerd32@gmail.com>
2015-01-10 14:00:04 -0500

    Third attempt to fix hound's howls

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,5 @@ require "rails_helper"
 RSpec.describe User, :type => :model do
   let(:blank_values) { [nil, ""] }
   it { should have_valid(:username).when("Any 1 .!@#$%^&*}text") }
-  it { should_not have_valid(:username).when(*blank_values)}
+  it { should_not have_valid(:username).when(*blank_values) }
 end
