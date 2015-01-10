b89039e0e889961a5c249524eb0e048badc7f7f6
dancernerd32 <dancernerd32@gmail.com>
2015-01-10 13:53:19 -0500

    First attempt to fix hound's howls

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   validates :username,
             presence: true,
             uniqueness: {
-              :case_sensitive => false
+              case_sensitive: false
             }
 
   def self.find_first_by_auth_conditions(warden_conditions)
@@ -17,8 +17,8 @@ class User < ActiveRecord::Base
     if login = conditions.delete(:login)
       where(conditions).where([
         "lower(username) = :value OR lower(email) = :value",
-        { :value => login.downcase }
-        ]).first
+        { value: login.downcase }
+      ]).first
     else
       if conditions[:username].nil?
         where(conditions).first
