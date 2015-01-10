7ca78f345e62671954d0941a9759db0d97a23f68
dancernerd32 <dancernerd32@gmail.com>
2015-01-10 13:16:04 -0500

    Follows devise wiki to add username to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :login
+
+  validates :username,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where([
+        "lower(username) = :value OR lower(email) = :value",
+        { :value => login.downcase }
+        ]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
 end
