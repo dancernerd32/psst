840b99f0e38fe9cc43c74c737415fe0a0cd7a1d1
dancernerd32 <dancernerd32@gmail.com>
2015-01-13 12:11:53 -0500

    Adds model testing to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  validates :body, presence: true, length: {maximum: 255}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe User, :type => :model do
+RSpec.describe User, type: :model do
   let(:blank_values) { [nil, ""] }
   it { should have_valid(:username).when("Any 1 .!@#$%^&*}text") }
   it { should_not have_valid(:username).when(*blank_values) }
